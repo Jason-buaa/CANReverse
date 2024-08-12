1e332655e2a7395db3fa40e3f30027f225b38452
Jason_buaa <waterworth@qq.com>
2024-08-12 22:57:54 +0800

    first release

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global console, document, Excel, Office */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Excel) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+  }
+});
+
+export async function run() {
+  try {
+    await Excel.run(async (context) => {
+      let currentSheet = context.workbook.worksheets.getActiveWorksheet();
+      let traceTable = currentSheet.tables.getItem("Trace");
+      // Get data from the header row.
+      let headerRange = traceTable.getHeaderRowRange().load("values");
+    await context.sync();
+
+    let headerValues = headerRange.values;
+    let headerValues2 = headerRange.values[0];
+    console.log(headerValues);
+    console.log(headerValues2);
+    //You can iterate through headers and perform operations on specific columns based on their names:
+    headerValues2.forEach(async (header, index) => {
+      if (header === "ID") {
+          // Perform operations on the "ID" column
+          let idColumnRange = traceTable.getDataBodyRange().getColumn(index).load("values");
+          await context.sync();
+          
+          let count = idColumnRange.values.flat().filter(value => value === 64).length;
+          console.log(`Count of rows with ID=64: ${count}`);
+      }
+  });
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
