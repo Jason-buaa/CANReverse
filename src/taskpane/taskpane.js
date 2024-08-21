43b352fde1c9233ea19b28871c55e97ba4d93ab9
Jason_buaa <waterworth@qq.com>
2024-08-21 16:01:43 +0800

    extract all data bytes to an array.

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -34,18 +34,33 @@ export async function run() {
           let idColumnRange = traceTable.getDataBodyRange().getColumn(index).load("values,rowIndex,rowCount");
           let timestampColumnRange = traceTable.getDataBodyRange().getColumn(index-2).load("values,rowIndex,rowCount");
           let byte0ColumnRange = traceTable.getDataBodyRange().getColumn(index+4).load("values,rowIndex,rowCount");
+          let byte1ColumnRange = traceTable.getDataBodyRange().getColumn(index+5).load("values,rowIndex,rowCount");
+          let byte2ColumnRange = traceTable.getDataBodyRange().getColumn(index+6).load("values,rowIndex,rowCount");
+          let byte3ColumnRange = traceTable.getDataBodyRange().getColumn(index+7).load("values,rowIndex,rowCount");
+          let byte4ColumnRange = traceTable.getDataBodyRange().getColumn(index+8).load("values,rowIndex,rowCount");
+          let byte5ColumnRange = traceTable.getDataBodyRange().getColumn(index+9).load("values,rowIndex,rowCount");
+          let byte6ColumnRange = traceTable.getDataBodyRange().getColumn(index+10).load("values,rowIndex,rowCount");
+          let byte7ColumnRange = traceTable.getDataBodyRange().getColumn(index+10).load("values,rowIndex,rowCount");
           await context.sync();
           let idValues =idColumnRange.values.flat();
           let id64RowIndices = idValues
           .map((el, idx) => el === 64 ? idx : -1) // +2 to adjust for Excel row numbers (assuming table starts at row 1)
           .filter(index => index !== -1); // Filter out the -1 values
           let id64Timestamps= id64RowIndices.map(idx => timestampColumnRange.values[idx]);
-          let id64Bytes0=id64RowIndices.map(idx=>byte0ColumnRange.values[idx]).map(hexStr=>parseInt(hexStr,16));
-      let count = id64RowIndices.length;
-      console.log(`Count of rows with ID=64: ${count}`);
-      console.log(`Row indices with ID=64: ${id64RowIndices}`);
-      console.log(`Row indices with ID=64: ${id64Timestamps}`);
-      console.log(`Row indices with ID=64: ${id64Bytes0}`);
+          let id64Bytes0=id64RowIndices.map(idx=>byte0ColumnRange.values[idx]).map(hexStr=>parseInt(hexStr,16).toString(2).padStart(8,'0'));
+          let id64Bytes1=id64RowIndices.map(idx=>byte1ColumnRange.values[idx]).map(hexStr=>parseInt(hexStr,16).toString(2).padStart(8,'0'));
+          let id64Bytes2=id64RowIndices.map(idx=>byte2ColumnRange.values[idx]).map(hexStr=>parseInt(hexStr,16).toString(2).padStart(8,'0'));
+          let id64Bytes3=id64RowIndices.map(idx=>byte3ColumnRange.values[idx]).map(hexStr=>parseInt(hexStr,16).toString(2).padStart(8,'0'));
+          let id64Bytes4=id64RowIndices.map(idx=>byte4ColumnRange.values[idx]).map(hexStr=>parseInt(hexStr,16).toString(2).padStart(8,'0'));
+          let id64Bytes5=id64RowIndices.map(idx=>byte5ColumnRange.values[idx]).map(hexStr=>parseInt(hexStr,16).toString(2).padStart(8,'0'));
+          let id64Bytes6=id64RowIndices.map(idx=>byte6ColumnRange.values[idx]).map(hexStr=>parseInt(hexStr,16).toString(2).padStart(8,'0'));
+          let id64Bytes7=id64RowIndices.map(idx=>byte7ColumnRange.values[idx]).map(hexStr=>parseInt(hexStr,16).toString(2).padStart(8,'0'));
+          let combinedArray=id64Bytes0.map((element,index)=>element+id64Bytes1[index]+id64Bytes2[index]+id64Bytes3[index]+id64Bytes4[index]+id64Bytes5[index]+id64Bytes6[index]+id64Bytes7[index]);
+      //let count = id64RowIndices.length;
+      //console.log(`Count of rows with ID=64: ${count}`);
+      //console.log(`Row indices with ID=64: ${id64RowIndices}`);
+      //console.log(`Row indices with ID=64: ${id64Timestamps}`);
+      console.log(`Row indices with ID=64: ${combinedArray}`);
       }
   });
     });
